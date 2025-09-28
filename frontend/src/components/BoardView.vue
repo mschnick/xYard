f8dc2ae46594be6a494dc66941f1e99e270aa03f
Michael Schnick <michael.schnick@schnick.de>
2025-09-28 13:26:12 +0200

    show both dashboard and board

--- frontend/src/components/BoardView.vue
+++ frontend/src/components/BoardView.vue
@@ -2,19 +2,23 @@
 const widthMm = 500
 const heightMm = 700
 // todo: scale to container
+const pxPerMm = 3.78 // 96dpi
+const widthPx = widthMm * pxPerMm
+const heightPx = heightMm * pxPerMm
 </script>
 
 <template>
   <div class="w-full h-full overflow-auto bg-green-50">
     <div
-      class="relative bg-white border"
+      class="relative bg-white border border-blue-500"
       :style="{
-        width: widthMm + 'mm',
-        height: heightMm + 'mm'
+        width: widthPx + 'px',
+        height: heightPx + 'px'
       }"
     >
-      <!-- später: Spielfeldinhalte -->
-       Ich bin das Spielfeld
+      <div style="width: 100%; height: 100%; background: red;">
+        Ich bin das Spielfeld
+      </div>
     </div>
   </div>
 </template>
--- frontend/src/pages/GamePage.vue
+++ frontend/src/pages/GamePage.vue
@@ -1,11 +1,18 @@
 <template>
-    <div class="flex h-screen overflow-hidden">
-      <BoardView class="flex-1 bg-gray-100" />
-      <GameDashboard class="w-96 bg-white border-l shadow-lg" />
+  <div class="flex h-screen overflow-hidden">
+    <!-- Board soll flexibel Platz einnehmen -->
+    <div class="flex-1 bg-gray-100 overflow-hidden">
+      <BoardView />
     </div>
+
+    <!-- Dashboard mit fixer Breite -->
+    <div class="w-96 border-l shadow-lg bg-white">
+      <GameDashboard />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-  import BoardView from '@/components/BoardView.vue'
-  import GameDashboard from '@/components/GameDashboard.vue'
+import BoardView from '@/components/BoardView.vue'
+import GameDashboard from '@/components/GameDashboard.vue'
 </script>
